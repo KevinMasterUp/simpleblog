7622e7755d3476c7a83fb6c5ee955845fe2740f7
Kevin <757625059@qq.com>
2016-03-03 12:28:19 +0800

    modify

--- simpleblog/app/database/migrations/2016_03_03_035014_create_users_table.php
+++ simpleblog/app/database/migrations/2016_03_03_035014_create_users_table.php
@@ -15,19 +15,16 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('email')->nullable();
+			$table->string('email');
 			$table->string('password');
-			$table->increments('activited');
-			$table->string('actioncode');
-			$table->text('permissions');
-			$table->timestamps('activited_at');
-			$table->timestamps('last_login');
-			$table->string('persist_code');
-			$table->string('reset_password_code');
-			$table->string('first_name');
-			$table->string('last_name');
-			$table->timestamps('create_at');
-			$table->timestamps('update_at');
+			$table->integer('activited')->nullable();
+			$table->string('actioncode')->nullable();
+			$table->text('permissions')->nullable();
+			$table->string('persist_code')->nullable();
+			$table->string('reset_password_code')->nullable();
+			$table->string('first_name')->nullable();
+			$table->string('last_name')->nullable();
+			$table->timestamps();
 		});
 	}
 
